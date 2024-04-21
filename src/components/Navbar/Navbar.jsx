e9a19519b8fe4568144a34d4021b27b5b90d7a92
Fawazie <fawazabdulwahab@gmail.com>
2024-04-20 21:46:31 -0500

    updated nav bar and hero code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,10 +4,10 @@ const Navbar = () => {
         <div className='nav'>
             <div className='nav-logo'>The M3 Evolution</div>
             <ul className='nav-menu'>
-                <li className='home'>Home</li>
-                <li className='explore'>Explore</li>
-                <li className='about'>About</li>
-                <li className='nav-contact'>Contact</li>
+                <li className='home background1'>Home</li>
+                <li className='explore background2'>Explore</li>
+                <li className='about background3'>About</li>
+
             </ul>
         </div>
     )
